f6f84a6ff0582ab896f2b0ddc84c831bf95cbab0
imciner2 <Ian.S.McInerney@ieee.org>
2022-01-15 16:33:38 +0000

    Deploying to gh-pages from @ imciner2/imciner2.github.io@ef02d1633ce302e4f86e1a02003e7b93c3b15080 🚀

---
+++ jekyll-scholar-extras/lib/jekyll/scholar/extras/bibliography_bytype.rb
@@ -0,0 +1,104 @@
+module Jekyll
+  class Scholar
+
+    class BibliographyTagByType < Liquid::Tag
+      include Scholar::Utilities
+      include ScholarExtras::Utilities 
+
+      def initialize(tag_name, arguments, tokens)
+        super
+
+        @config = Scholar.defaults.dup
+        
+        optparse(arguments)
+      end
+
+      def initialize_type_labels()
+        @type_labels =
+          Hash[{ "@article" => "Peer-reviewed Journal Articles",
+                 "@inproceedings" => "Peer-reviewed Conference and Workshop Papers",
+                 "@incollection" => "Book Chapters",
+                 "@techreport" => "Technical Reports",
+                 "@book" => "Books",
+                 "@thesis" => "Thesis",
+                 "@patent" => "Patents",
+                 "@unpublished" => "Preprints"
+               }]
+      end
+
+
+      def set_type_counts(tc)
+        @type_counts = tc
+      end
+
+      def render_index(item, ref)
+        si = '[' + @prefix_defaults[item.type].to_s + @type_counts.to_s + ']'
+        @type_counts = @type_counts - 1
+        
+        idx_html = content_tag "div", si, { :class => "csl-index"}
+        return idx_html + ref
+      end
+
+      def render_header(y)
+        ys = content_tag "h2", y, { :class => "csl-year-header" }
+        ys = content_tag "div", ys, { :class => "csl-year-icon" }
+      end
+
+      def render(context)
+        set_context_to context
+
+        # Only select items that are public.
+        items = entries.select { |e| entry_is_public(e) }
+
+        initialize_prefix_defaults()
+        initialize_type_labels()
+        set_type_counts(items.size())
+
+        if cited_only?
+          items =
+            if skip_sort?
+              cited_references.uniq.map do |key|
+              items.detect { |e| e.key == key }
+            end
+            else entries.select  do |e|
+              cited_references.include? e.key
+            end
+            end
+        end
+
+        items = items[offset..max] if limit_entries?
+
+        bibliography = render_header(@type_labels[query])
+        bibliography << items.each_with_index.map { |entry, index|
+          reference = render_index(entry, bibliography_tag(entry, nil))
+
+          if generate_details?
+            reference << link_to(details_link_for(entry), config['details_link'],
+                                 :class => config['details_link_class'])
+          end
+
+          if entry.field?(:award)
+            # TODO: Awkward -- Find position to insert it. Before the last </div>
+            ts = content_tag "div", entry.award.to_s, { :class => "csl-award" }
+            reference_position = reference.rindex('</div>')
+            if reference_position.nil?
+              puts "NILL"
+            else 
+              reference.insert(reference.rindex('</div>'), ts.to_s)
+            end 
+          end
+          
+          content_tag config['bibliography_item_tag'], reference
+          content_tag "li", reference, { :class => render_ref_img(entry) }
+        }.join("\n")
+
+
+        content_tag config['bibliography_list_tag'], bibliography, :class => config['bibliography_class']
+        
+      end
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('bibliography_bytype', Jekyll::Scholar::BibliographyTagByType)
---
+++ jekyll-scholar-extras/lib/jekyll/scholar/extras/bibliography_year.rb
@@ -0,0 +1,118 @@
+module Jekyll
+  class Scholar
+    
+    class BibliographyTagYear < Liquid::Tag
+      include Scholar::Utilities
+      include ScholarExtras::Utilities
+
+      def initialize(tag_name, arguments, tokens)
+        super
+
+        @config = Scholar.defaults.dup
+        @config_extras = ScholarExtras.extra_defaults.dup        
+
+        #puts @config_extras
+
+        #puts @config_extras['parse_extra_fields']
+        
+        optparse(arguments)
+
+      end
+
+      def initialize_type_counts()
+        @type_counts = Hash[{ :article => 0,
+                              :inproceedings => 0,
+                              :incollection=> 0,
+                              :techreport => 0,
+                              :book => 0,
+                              :unpublished => 0,
+                              :patent => 0,
+                              :thesis => 0
+                            }]
+
+        @type_counts.keys.each { |t|
+          bib = bibliography.query('@*') { |b|
+            (entry_is_public(b) && b.type == t)
+          }
+          @type_counts[t] = bib.size
+        }
+      end
+
+      def initialize_type_order()
+        @type_order = Hash[{}]
+        entries.each{ |i| @type_order[i.type] = 0}
+      end
+
+
+      def get_entries_by_type(year, type)
+        b = bibliography.query('@*') { |item|
+          (item.year == year && item.type == type)
+        }
+      end
+
+      def render_year(y)
+        ys = content_tag "h2", y, { :class => "csl-year-header" }
+        ys = content_tag "div", ys, { :class => "csl-year-icon" }
+      end
+
+
+      def entries_year(year)
+        b = bibliography.query('@*') { 
+          |a| (a.year == year && entry_is_public(a))
+        }
+      end
+
+      def initialize_unique_years
+        # Get an array of years and then uniquify them.
+        items = entries
+        arr = Array.new
+        items.each { |i| arr.push(i.year.to_s)  }
+        @arr_unique = arr.uniq
+      end
+
+      def render(context)
+        set_context_to context
+
+        # Initialize the number of each type of interest.
+        initialize_type_counts()
+        initialize_type_order()
+        initialize_prefix_defaults()
+        initialize_unique_years()
+
+        # Iterate over unique years, and produce the bib.
+        bibliography =""
+        @arr_unique.each { |y|
+          bibliography << render_year(y)
+          @type_order.keys.each { |o|
+            items = entries_year(y).select { |e| e.type == o }
+            bibliography << items.each_with_index.map { |entry, index|
+              if entry.type == o then 
+                reference = render_index(entry, bibliography_tag(entry, nil))
+
+                if entry.field?(extra_parse_fields['award'])
+                  # TODO: Awkward -- Find position to insert it. Before the last </div>
+                  ts = content_tag "div", entry.award.to_s, { :class => "csl-award" }
+                  reference_position = reference.rindex('</div>')
+                  if reference_position.nil? 
+                  else 
+                    reference.insert( reference.rindex('</div>'), ts.to_s )
+                  end
+                end
+
+                # Content tag is dependent on type of article.
+                content_tag "li", reference, { :class => render_ref_img(entry) }
+              end
+#              split_reference reference                                    
+            }.join("\n")
+
+          }.join("\n")
+          
+
+        }.join("")
+        return content_tag config['bibliography_list_tag'], bibliography, :class => config['bibliography_class']
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('bibliography_year', Jekyll::Scholar::BibliographyTagYear)
